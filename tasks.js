26d01a46d801b45adcbe93db926874b9e16cf66e
Raaya Pathare <raaya.pathare7@gmail.com>
2019-06-03 11:06:55 +1200

    displays a list of artworks

--- tasks.js
+++ tasks.js
@@ -2,8 +2,11 @@ function welcomeMessage (name) {
   return (`Welcome ${name}!`)
 }
 
-
+function displayArtworks (array) {
+  return array.join(', ')
+}
 
 module.exports = {
-  welcomeMessage
+  welcomeMessage,
+  displayArtworks
 }
--- tests/init.test.js
+++ tests/init.test.js
@@ -5,11 +5,21 @@ test('test setup working', () => {
 })
 
 test('should display a welcome message', () => {
-  const name = 'Harry'
   // arrange
-  // act
+  const name = 'Harry'
   const expected = 'Welcome Harry!'
-  // assert
+  // act
   const actual = tasks.welcomeMessage(name)
+  // assert
+  expect(actual).toBe(expected)
+})
+
+test('should display list of ASCII artworks', () => {
+  // arrange
+  const artworks = ['kea', 'kiwi', 'nikau', 'pohutukawa']
+  const expected = 'kea, kiwi, nikau, pohutukawa'
+  // act
+  const actual = tasks.displayArtworks(artworks)
+  // assert
   expect(actual).toBe(expected)
 })
