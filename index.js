9f7b757455f68c12f83021199d8539a7db994390
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-06-03 12:28:34 +1200

    done step 2

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-module.exports = { returnFive: returnFive }
+const fs = require('fs')
+const artFiles = []
 
 function returnFive () {
   return 5
+}
+
+function welcome (name) {
+  return 'welcome ' + name
+}
+
+function artFilesPushData (er, dt) {
+  if (er) {
+    console.error(er.message)
+  } else {
+    console.log(dt)
+    return artFiles.push(dt)
+  }
+}
+
+function getAss (callback) {
+  let data = fs.readdirSync('data/')
+
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[i])
+    const file = `data/${data[i]}`
+    fs.readFile(file, 'utf-8', callback)
+  }
+}
+function showDatAss (artwork) {
+  // get  files in array
+  // loop through and console.log each file
+
+  console.log(artwork)
+}
+
+getAss(artFilesPushData)
+
+module.exports = {
+  returnFive,
+  welcome,
+  getAss,
+  showDatAss
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,32 @@
 const inital = require('../index')
+const fs = require('fs')
 
-test('', () => {
+test('initisl test', () => {
   const expected = 5
   const actual = inital.returnFive()
   expect(actual).toBe(expected)
+})
+
+test('welcome user test', () => {
+  // arrange
+  const user = 'tamari'
+  const expected = `welcome ${user}`
+
+  // act
+  const actual = inital.welcome(user)
+
+  // assert
+  expect(expected).toBe(actual)
+})
+
+test('show single artwork test', () => {
+  // arrange
+  const kea = 'kea.txt'
+  let expected = ''
+  fs.readFile('../data/kea.txt', function read (err, data) { if (err) { throw err } expected = data })
+  // act
+  const actual = inital.getAss(kea)
+
+  // assert
+  expect(expected).toBe(actual)
 })
